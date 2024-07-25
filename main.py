ab7fd96cddb3528a4a34e8d8afe9fa5a50a316ab
Fedt2407 <federico.tognetti@gmail.com>
2024-07-25 12:22:12 +0200

    Added trend line of num of mission per year.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 load_dotenv()
 
@@ -13,7 +16,20 @@ def hello_world():
     data = pd.read_csv('./static/mission_launches.csv', index_col=0)
     shape = data.shape
     head = data.head().iloc[:, 1:].to_html(classes='dataframe', index=False)
-    return render_template('index.html', shape=shape, head=head)    
+
+    # Plotting the number of missions per year
+    # Extract the year from the Date column
+    data['Date'] = pd.to_datetime(data['Date'], errors='coerce', infer_datetime_format=True)
+    data['Year'] = data['Date'].dt.year
+    # Counts the number of missions per year
+    missions_per_year = data['Year'].value_counts().sort_index()
+    # Create a bar plot
+    trend = px.line(missions_per_year, x=missions_per_year.index, y=missions_per_year.values)
+    trend.update_layout(xaxis_title='Year', yaxis_title='Number of Missions')
+    trend.update_yaxes(range=[0, 120])
+    trend_html = trend.to_html(full_html=False)
+   
+    return render_template('index.html', shape=shape, head=head, trend=trend_html)    
 
 
 if __name__ == '__main__':
